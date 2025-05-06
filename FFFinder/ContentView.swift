f58664f9fc2ab972b37b25e9a6551afb6be65226
ZZ <zzwzzwzz@ZZmac.local>
2025-05-06 22:16:08 +1000

    Add basic template

---
+++ FFFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FFFinder
+//
+//  Created by ZZ on 2025.05.06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
