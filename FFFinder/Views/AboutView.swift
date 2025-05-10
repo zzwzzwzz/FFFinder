a4cc5a1e52075d3b0d9d210401f2e189f41aa231
APPLE <apple@MacBook-Pro-3.lan>
2025-05-11 09:51:20 +1000

    Add AboutView screen and adjust profile navigation

---
+++ FFFinder/Views/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  FFFinder
+//
+//  Created by APPLE on 2025/5/11.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
